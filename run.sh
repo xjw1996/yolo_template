8dddbf0e25ac586e3209ae69682066e98e18c6ca
xjw1996 <50350039+xjw1996@users.noreply.github.com>
2022-04-21 03:31:23 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 while true
 do
-./darknet detector train ./path.conf ./yolov3-voc.cfg ./weights/yolov3-voc.backup
+./darknet detector train /home/xue/darknet/gomi/path.conf /home/xue/darknet/gomi/yolov3-voc.cfg /home/xue/darknet/gomi/weights/yolov3-voc.backup
 done
